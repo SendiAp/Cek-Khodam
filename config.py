9f261b343ad055949573bebca3dc852e99a0c781
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-22 16:58:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ from os import getenv
 API_ID = getenv("API_ID", "")
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+MONGO_DB_URL = getenv("MONGO_DB_URL", "")
+DB_NAME = getenv("DB_NAME", "kode")
