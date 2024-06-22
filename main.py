20a7a06522041f002bfbdc2afd895709f7c3cab7
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-22 18:35:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,9 @@ async def cekkhodam(bot : Client, message : Message):
 
     if khodam in kosong:
         return await await bot.send_photo(chat_id, f"photo/{khodam}.jpg", caption=txt)
-        
+
+    await xx.edit(f"🔍 Sedang Melihat Khodam {msg} ....")
+    
     try: 
         await bot.send_photo(chat_id, f"photo/{khodam}.jpg", caption=text)
         await xx.delete()
