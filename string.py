d24920ef0d2ddecfe5c6bcd1c427919e40cc85ef
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-22 11:59:46 +0700

    Update string.py

--- string.py
+++ string.py
@@ -12,13 +12,13 @@ Khodam = [
   "Cermin Retak"
 ]
 
-Pendamping Para Raja = "https://telegra.ph//file/6d9664766fbdb75a2ccca.jpg"
-Pendamping Para Ratu Dan Putri = "https://telegra.ph//file/1c4bf541590a66901b665.jpg"
-Naga = "https://telegra.ph//file/f05e380481daee9738cd9.jpg"
-Harimau Putih = "https://telegra.ph//file/75459598178c8d51edaa0.jpg"
-Buaya Putih = "https://telegra.ph//file/19d24fd99e97cb88898d4.jpg"
-Macan Tutul = "https://telegra.ph//file/d47160bf296bfe024ea0c.jpg"
-Ratu Ular = "https://telegra.ph//file/202778df13e12cb652ce1.jpg"
-Stoples Kaca = "https://telegra.ph//file/6a0a6100cab573248ecac.jpg"
-Honda Verza = "https://telegra.ph//file/86571ce13c3a8a1eaeb0a.jpg"
-Cermin Retak = "https://telegra.ph//file/d886549e633148b515a1e.jpg"
+Pendamping Para Raja = ["https://telegra.ph//file/6d9664766fbdb75a2ccca.jpg"]
+Pendamping Para Ratu Dan Putri = ["https://telegra.ph//file/1c4bf541590a66901b665.jpg"]
+Naga = ["https://telegra.ph//file/f05e380481daee9738cd9.jpg"]
+Harimau Putih = ["https://telegra.ph//file/75459598178c8d51edaa0.jpg"]
+Buaya Putih = ["https://telegra.ph//file/19d24fd99e97cb88898d4.jpg"]
+Macan Tutul = ["https://telegra.ph//file/d47160bf296bfe024ea0c.jpg"]
+Ratu Ular = ["https://telegra.ph//file/202778df13e12cb652ce1.jpg"]
+Stoples Kaca = ["https://telegra.ph//file/6a0a6100cab573248ecac.jpg"]
+Honda Verza = ["https://telegra.ph//file/86571ce13c3a8a1eaeb0a.jpg"]
+Cermin Retak = ["https://telegra.ph//file/d886549e633148b515a1e.jpg"]
