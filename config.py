98ef13ca5626f8d2b280a980deff98fb6c82f701
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-22 18:54:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,14 @@ from os import getenv
 
 API_ID = getenv("API_ID", "")
 API_HASH = getenv("API_HASH", "")
+
 BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
 
 MONGO_DB_URL = getenv("MONGO_DB_URL", "")
 DB_NAME = getenv("DB_NAME", "khodam")
+
+FORCE_SUB_CHANNEL = getenv("FORCE_SUB_CHANNEL", "")
+FORCE_SUB_GROUP = getenv("FORCE_SUB_GROUP", "")
 
 BROADCAST_AS_COPY = True
