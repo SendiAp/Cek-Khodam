df87dec9c78fb01d43a4d48e760087d0ec113d8f
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2024-06-22 16:08:53 +0700

    Update picture.py

--- picture.py
+++ picture.py
@@ -11,14 +11,3 @@ Pasukan = [
   "HondaVerza",
   "CerminRetak"
 ]
-
-PendampingParaRaja = "https://telegra.ph//file/6d9664766fbdb75a2ccca.jpg"
-PendampingParaRatu = "https://telegra.ph//file/1c4bf541590a66901b665.jpg"
-Naga = "https://telegra.ph//file/f05e380481daee9738cd9.jpg"
-HarimauPutih = "https://telegra.ph//file/75459598178c8d51edaa0.jpg"
-BuayaPutih = "https://telegra.ph//file/19d24fd99e97cb88898d4.jpg"
-MacanTutul = "https://telegra.ph//file/d47160bf296bfe024ea0c.jpg"
-RatuUlar = "https://telegra.ph//file/202778df13e12cb652ce1.jpg"
-StoplesKaca = "https://telegra.ph//file/6a0a6100cab573248ecac.jpg"
-HondaVerza = "https://telegra.ph//file/86571ce13c3a8a1eaeb0a.jpg"
-CerminRetak = "https://telegra.ph//file/d886549e633148b515a1e.jpg"
